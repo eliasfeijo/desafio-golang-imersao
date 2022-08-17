de95e6610324c6b797d960866d5fdd58321f2db4
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-08-17 20:23:25 -0300

    adding mux dependency and starting server at port 4000

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/eliasfeijo/desafio-imersao
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello")
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode("Hello World")
+	})
+
+	log.Println("API is running")
+	http.ListenAndServe(":4000", router)
 }
