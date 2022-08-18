ae80b21d6c5e745fd7c35edb03a3c9566158afdb
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-08-18 10:08:09 -0300

    database connection and migrate

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/eliasfeijo/desafio-imersao
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.15
+)
--- main.go
+++ main.go
@@ -4,11 +4,26 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/eliasfeijo/desafio-imersao/database"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	db, err := database.ConnectDatabase()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	err = database.Migrate()
+	if err != nil {
+		log.Fatalf("Error migrating database: %v", err)
+		os.Exit(1)
+	}
+
 	router := mux.NewRouter()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode("Hello World")
